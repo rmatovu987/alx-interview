2494c9662f85975430a44bbf5d770a86d478d859
rmatovu987 <rmatovu987@gmail.com>
2022-04-20 16:33:14 +0300

    minimum_operations

--- 0x02-minimum_operations/0-minoperations.py
+++ 0x02-minimum_operations/0-minoperations.py
@@ -6,12 +6,15 @@ number of operations needed to result in exactly n H characters in the file.
 
 
 def minOperations(n):
-    """Calculate the fewest no. of operations needed to result in n H characters"""
-    t = 0
-    m = 2
+    """Returns number of the fewest iterations possible"""
+    if n <= 1:
+        return 0
+    mod = 2
+    iterations = 0
     while n > 1:
-        while not n % m:
-            t += m
-            n /= m
-        m += 1
-    return t
+        if n % mod == 0:
+            iterations = iterations+mod
+            n = n/mod
+        else:
+            mod = mod + 1
+    return iterations
