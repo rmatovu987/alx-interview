2f4c863ae287eb0ae37613e05fd5e26137bb1571
rmatovu987 <rmatovu987@gmail.com>
2022-05-19 13:56:02 +0300

    starwars

---
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -0,0 +1,24 @@
+#!/usr/bin/node
+
+const request = require('request');
+const dict = [];
+let i = 0;
+request('https://swapi-api.hbtn.io/api/films/' + process.argv[2], (ferr, fresponse, fbody) => {
+    if (ferr) return console.log(ferr);
+    for (const chr of JSON.parse(fbody).characters) {
+        request(chr, (cerr, cresponse, cbody) => {
+            if (cerr) return console.log(cerr);
+            const character = JSON.parse(cbody);
+            const words = chr.split('/');
+            const id = words[5];
+            if (!dict[id]) {
+                dict[id] = [];
+            }
+            dict[id].push(character.name);
+            if (i === JSON.parse(fbody).characters.length - 1) {
+                for (const ele of dict) if (ele) console.log(ele[0]);
+            }
+            i++;
+        });
+    }
+});
